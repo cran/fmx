7b0a3675a3d91ba81cc11c4569bd2e29b1a90bd8
Tingting Zhan <tingtingzhan@gmail.com>
2024-03-25 19:00:02 +0000

    version 0.1.0

---
+++ R/0PACKAGE.R
@@ -0,0 +1,27 @@
+
+
+#' @title Finite Mixture Parametrization
+#'
+#' @description
+#' ..
+#' 
+#' @returns 
+#' Returns of each function will be documented separately.
+#'
+#' @author Tingting Zhan
+#'
+#' @keywords internal
+'_PACKAGE'
+
+
+
+# @name fmx-package
+#NULL
+
+
+
+
+
+
+
+
